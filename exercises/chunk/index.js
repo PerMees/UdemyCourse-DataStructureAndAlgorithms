0783f04cd866cc011ca2e54bb7aa1e66bb49f976
Phuc <phucle@Phucs-MacBook-Air.local>
2021-11-23 15:37:45 +0700

    done chunk array

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,25 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  let resultArr = [];
+  //   let count = 0;
+  //   let tempArr = [];
+  //   for (let i = 0; i < array.length; i++) {
+  //     count++;
+  //     tempArr.push(array[i]);
+  //     if (count === size) {
+  //       count = 0;
+  //       resultArr.push(tempArr);
+  //       tempArr = [];
+  //     } else if (i === array.length - 1) resultArr.push(tempArr);
+  //   }
+  let index = 0;
+  while (index < array.length) {
+    resultArr.push(array.slice(index, index + size));
+    index += size;
+  }
+  return resultArr;
+}
 
 module.exports = chunk;
