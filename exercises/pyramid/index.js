1913ed22a64024ddb61cc9b996796af72f30c947
Phuc <phucle@Phucs-MacBook-Air.local>
2021-11-24 21:41:41 +0700

    done pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -10,10 +10,35 @@
 //       ' # '
 //       '###'
 //   pyramid(3)
-//       '  #  '
-//       ' ### '
-//       '#####'
+//       '  #  '  0 1, 3 4   3-1 = 2
+//       ' ### '  0, 4     3-2 = 1
+//       '#####' 3-row
 
-function pyramid(n) {}
+// function pyramid(n, row = 0, stair = "") {
+//   const maxlength = n * 2 - 1;
+//   if (row === n) {
+//     return;
+//   }
+//   if (stair.length === maxlength) {
+//     console.log(stair);
+//     return pyramid(n, row + 1);
+//   }
+//   if (stair.length < n - row - 1 || stair.length >= maxlength - (n - row - 1))
+//     stair += " ";
+//   else stair += "#";
+//   return pyramid(n, row, stair);
+// }
+
+function pyramid(n) {
+  const maxlength = n * 2 - 1;
+  for (let i = 0; i < n; i++) {
+    let stair = "";
+    for (let j = 0; j < maxlength; j++) {
+      if (j < n - i - 1 || j >= maxlength - (n - i - 1)) stair += " ";
+      else stair += "#";
+    }
+    console.log(stair);
+  }
+}
 
 module.exports = pyramid;
