716b4a35a4dfba460193449b87709ea3f5b947f9
Phuc <phucle@Phucs-MacBook-Air.local>
2021-11-24 20:27:58 +0700

    done steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,31 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// function steps(n) {
+//   for (let i = 1; i <= n; i++) {
+//     let result = "";
+//     // for (let j = 1; j <= i; j++) {
+//     //   result += "#";
+//     // }
+//     // for (let k = i; k < n; k++) result += " ";
+
+//     for (let j = 1; j <= n; j++) {
+//       j <= i ? (result += "#") : (result += " ");
+//     }
+//     console.log(result);
+//   }
+// }
+
+function steps(n, row = 0, stair = "") {
+  if (row === n) {
+    return;
+  }
+  if (stair.length === n) {
+    console.log(stair);
+    return steps(n, row + 1);
+  }
+  stair.length <= row ? (stair += "#") : (stair += " ");
+  return steps(n, row, stair);
+}
 
 module.exports = steps;
