bb19689a120783eec7cb0bf0abfd81ed4ed041e9
Phuc <phucle@Phucs-MacBook-Air.local>
2021-11-25 22:24:26 +0700

    done vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,15 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  //   let sum = 0;
+  //   let arrCheck = ["a", "e", "i", "o", "u"];
+  //   for (let char of str) {
+  //     if (arrCheck.includes(char.toLowerCase())) sum += 1;
+  //   }
+  //   return sum;
+  const matches = str.match(/[aeiou]/gi);
+  return matches ? matches.length : 0;
+}
 
 module.exports = vowels;
