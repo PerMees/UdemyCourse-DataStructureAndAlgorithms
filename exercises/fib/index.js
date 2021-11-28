88c515346b47affe70d33b3c3a67743d605bc392
Le Dam Duy Phuc <phucle@Les-MacBook-Air.local>
2021-11-28 13:08:41 +0700

    done fib

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,37 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+// function fib(n, count = 3, prev1 = 1, prev2 = 1) {
+//   if (n < 3) return 1;
+//   if (count < n) {
+//     return fib(n, count + 1, prev2, prev1 + prev2);
+//   }
+//   return prev1 + prev2;
+// }
+// function fib(n) {
+//     const result = [0, 1];
+//     for (let i = 2; i <= n; i++) {
+//       const a = result[i - 1];
+//       const b = result[i - 2];
+//       result.push(a + b);
+//     }
+//     return result[n];
+//   }
+function memoize(fn) {
+  const cache = {};
+  return function (...args) {
+    if (cache[args]) return cache[args];
+    const result = fn.apply(null, args);
+    cache[args] = result;
+    return result;
+  };
+}
+
+function fib(n) {
+  if (n < 2) return n;
+  return fib(n - 1) + fib(n - 2);
+}
+
+fib = memoize(fib);
 
 module.exports = fib;
