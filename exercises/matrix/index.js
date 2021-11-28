36656867c1fecd3bc4ce2a6dc7bc493c0c3546da
Le Dam Duy Phuc <phucle@Les-MacBook-Air.local>
2021-11-28 09:10:01 +0700

    done matrix

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,41 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  let results = [],
+    counter = 1,
+    row = 0,
+    endRow = n - 1,
+    col = 0,
+    endCol = n - 1;
+  for (let i = 0; i < n; i++) results.push([]);
+  while (row <= endRow && col <= endCol) {
+    // ? horizontal left to right
+    for (let i = col; i <= endCol; i++) {
+      results[row][i] = counter;
+      counter++;
+    }
+    row++;
+    // ? vertical top to bottom
+    for (let j = row; j <= endRow; j++) {
+      results[j][endCol] = counter;
+      counter++;
+    }
+    endCol--;
+    // ? horizontal right to left
+    for (let i = endCol; i >= col; i--) {
+      results[endRow][i] = counter;
+      counter++;
+    }
+    endRow--;
+    // ? vertical bottom to top
+    for (let j = endRow; j >= row; j--) {
+      results[j][col] = counter;
+      counter++;
+    }
+    col++;
+  }
+  return results;
+}
 
 module.exports = matrix;
